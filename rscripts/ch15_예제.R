82cdebade47bc31079c34642304281b8fa0ee119
Dnadit <yuhyun.kang07@gmail.com>
2023-03-28 13:11:38 +0900

    230328

---
+++ rscripts/ch15_예제.R
@@ -0,0 +1,38 @@
+# 1. 통계청 출산 데이터 (시계열, 회귀)
+# 2. ~ 집 값 데이터(캘리포니아) => 대표적인 회귀
+# 3. 손글씨 분류 => 분류(0~9분류)
+# 예제 : 캘리포니아 집 값 데이터
+
+library(tidyverse)
+library(reshape2)
+
+house <- read.csv("./data/housing.csv")
+head(house)
+
+summary(house)
+
+## 데이터 시각화(데이터 확인을 위한)
+par(mfrow=c(2,5))
+colnames(house)
+#ggplot(data = melt(house), 
+#       mapping = aes(x = value) + geom_histogram(bins=30) + facet_wrap(variable, scale = 'free_x'))
+
+## 결측값 처리
+house$mean_bedrooms = house$total_bedrooms / house$households
+house$mean_rooms = house$total_rooms / house$households
+drop = c('total_bedrooms', 'total_rooms')
+house = house[ , !(names(house) %in% drop)]
+head(house)
+
+## 전처리(상식을 사용해서 가정에 대한 데이터를 별도로 분리)
+categories = unique(house$ocean_proximity)
+cat_house = data.frame(ocean_proximity = house$ocean_proximity)
+
+for(cat in categories) {
+  cat_house[,cat] = rep(0, times=nrow(cat_house))
+}
+for(i in 1:length(cat_house$ocean_proximity)) {
+  cat = as.character(cat_house$ocean_proximity[i])
+  cat_house[,cat][i]=1
+}
+cat_house
--- rscripts/chapter15.R
+++ rscripts/chapter15.R
@@ -1,179 +1,40 @@
 # Chapter 15
 
-# 실습: 단순 선형 회귀분석 수행
-# 단계 1: 데이터 가져오기 
-product <- read.csv("C:/Rwork/Part-III/product.csv", header = TRUE)
+# 실습 1: 단순 선형 회귀분석 수행
+## 단계 1: 데이터 가져오기 (전처리 NA)
+product <- read.csv("./data/product.csv", header = TRUE, fileEncoding = "euc-kr")
 str(product)
 
-# 단계 2: 독립변수와 종속벼수 생성
+## 단계 2: 독립변수와 종속변수 생성
 y = product$제품_만족도 
 x = product$제품_적절성
 df <- data.frame(x, y)
 
-# 단계 3: 단순 선형회귀 모델 생성
+## 단계 3: 단순 선형회귀 '모델' 생성
 result.lm <- lm(formula = y ~ x, data = df)
 
-# 단계 4: 회귀분석의 절편과 기울기
+## 단계 4: 회귀분석의 절편과 기울기
 result.lm
 
-# 단계 5: 모델의 적합값과 잔차 보기 
-names(result.lm)
-
-# 단계 5-1: 적합값 보기 
-fitted.values(result.lm)[1:2]
-
-# 단계 5-2: 관측값 보기 
-head(df, 1)
-
-# 단계 5-3: 회귀방정식을 적용하여 모델의 적합값 계산
-Y = 0.7789 + 0.7393 * 4
-Y
-
-# 단계 5-4: 잔차(오차) 계산
-3 - 3.735963
-
-
-# eksrP 5-5: 모델의 잔차 보기 
+## 단계 5: 모델의 잔차 보기 
 residuals(result.lm)[1:2]
 
-# 단계 5-6: 모델의 잔차와 회귀방정식에 의한 적합값으로부터 관측값 계산
--0.7359630 + 3.735963
-
-
-# 실습: 선형 회귀분석 모델 시각화
-# 단계 1: x, y 산점도 그리기 
+# 실습2: 선형 회귀분석 모델 시각화
+## 단계 1: x, y 산점도 그리기 
 plot(formula = y ~ x, data = product)
 
 
-# 단계 2: 선형 회귀모델 생성
+## 단계 2: 선형 회귀모델 생성
 result.lm <- lm(formula = y ~ x, data = product)
 
-# 단계 3: 회귀선
+## 단계 3: 회귀선
 abline(result.lm, col = "red")
 
 
-# 실습: 선형 회귀분석 결과보기 
+## 단계 4: 선형 회귀분석 결과보기 
 summary(result.lm)
 
-
-# 실습: 다중 회귀분ㄴ석
-# 단계 1: 변수 모델링
-y = product$제품_만족도
-x1 = product$제품_친밀도
-x2 = product$제품_적절성
-df <- data.frame(x1, x2, y)
-
-# 단계 2: 다중 회귀분석
-result.lm <- lm(formula = y ~x1 + x2, data = df)
-result.lm
-
-
-# 실습: 다중 공선성 문제 확인
-# 단계 1: 패키지 설치
-install.packages("car")
-library(car)
-
-# 단계 2: 분산팽창요인(VIF)
-vif(result.lm)
-
-
-# 실습: 다중 회귀분석 결과보기 
-summary(result.lm)
-
-
-
-# 실습: 다중 공선성 ㅁ누제 확인
-# 단계 1: 패키지 설치 및 데이터 로딩
-install.packages("car")
-library(car)
-
-data(iris)
-
-# 단계 2: iris 데이터 셋으로 다중 회귀분석
-model <- lm(formula = Sepal.Length ~ Sepal.Width + 
-              Petal.Length + Petal.Width, data = iris)
-vif(model)
-sqrt(vif(model)) > 2
-
-
-# 단계 3: iris 변수 간의 상관계수 구하기 
-cor(iris[ , -5])
-
-
-
-# 실습: 데이터 셋 생성과 회귀모델 생성
-# 단계 1: 학습데이터와 검저엗이터 표본 추출
-x <-sample(1:nrow(iris), 0.7 * nrow(iris))
-train <- iris[x, ]
-test <- iris[-x, ]
-
-# 단계 2: 변수 제거 및 다중 회귀분석
-model <- lm(formula = Sepal.Length ~ Sepal.Width + Petal.Length, data = train)
-model
-summary(model)
-
-
-
-# 실습: 회귀방정식 도출
-# 단계 1: 회귀방정식을 위한 절편과 기울기 보기
-model
-
-# 단계 2: 회귀방정식 도출
-head(train, 1)
-# 다중 회귀방정식 적용
-Y = 2.3826 +  0.5684 * 2.9 + 0.4576 * 4.6
-Y
-6.6 - Y
-
-
-
-# 실습: 검정데이터의 독립변수를 이용한 예측치 생성
-pred <- predict(model, test)
-pred
-
-
-
-# 실습: 상관계수를 이용한 회귀모델 평가
-cor(pred, test$Sepal.Length)
-
-
-# 실습: 회귀분석의 기본 가정 충족으로 회귀분석 수행
-# 단계 1: 회귀모델 생성
-# 단계 1-1: 벼수 모델링
-formula = Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width
-
-# 단계 1-2: 회귀모델 생성
-model <- lm(formula = formula, data = iris)
-model
-
-# 단계 2: 잔차(오차) 부석
-# 단계 2-1: 독립성 검정 - 더빈 왓슨 값으로 확인
-install.packages('lmtest')
-library(lmtest)
-dwtest(model)
-
-
-# 단계 2-2: 등분산성 검정 - 잔차와 적합값의 분포
-plot(model, which = 1)
-
-# 단계 2-3: 잔차의 정규성 검정
-attributes(model)
-res <- residuals(model)
-shapiro.test(res)
-par(mfrow = c(1, 2))
-hist(res, freq = F)
-qqnorm(res)
-
-
-# 단계 3: 다중 공선성 검사
-library(car)
-sqrt(vif(model)) > 2
-
-# 단계 4: 회귀모델 생성과 평가 
-formula = Sepal.Length ~ Sepal.Width + Petal.Length
-model <- lm(formula = formula, data = iris)
-summary(model)
-
+# ------ 로지스틱 ------
 
 # 실습: 날씨 관련 요인 변수로 비(rain) 유뮤 예측
 # 단계 1: 데이터 가져오기 
